ebb3c330c42dba4ec2d9c66bb1c70b3651921d83
Rishabh Singh <vineetsatya36@gmail.com>
2024-04-19 03:47:15 +0530

    export import of functions

---
+++ features.js
@@ -0,0 +1,10 @@
+// const gfName = "Mrs Random";
+
+// export default gfName;
+
+const randomLovePercent = () => {
+  const randomPercentage = Math.random() * 100;
+  return `${~~randomPercentage}%`;
+};
+
+export default randomLovePercent;
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const http = require("http");
+import http from "http";
+// import gfName from "./features.js";
+import randomLovePercent from "./features.js";
 
-const server = http.createServer(() => {
-  console.log("Servered");
+// console.log(gfName);
+console.log(randomLovePercent());
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("<h1>This is Home page</h1>");
+  } else if (req.url === "/about") {
+    res.end("<h1>This is About page</h1>");
+  } else if (req.url === "/contact") {
+    res.end("<h1>Contact page</h1>");
+  } else {
+    res.end("404: Page Not Found");
+  }
 });
 
 server.listen(5000, () => {

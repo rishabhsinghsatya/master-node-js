6d11502c875c62b90f2877ba3a4bca9f727e2f59
Rishabh Singh <vineetsatya36@gmail.com>
2024-04-20 00:47:48 +0530

    ejs fundamentals

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 import express from "express";
-// import fs from "fs";
 import path from "path";
 
 const app = express();
 
+const users = [];
+
+//middleware
+app.use(express.static(path.join(path.resolve(), "public"))); //index file from public folder
+// console.log(path.join(path.resolve(), "public"));
+app.use(express.urlencoded({ extended: true }));
+
+//make sure folder name is "views"
+//you want do one thing, if set here or use extension on render
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  //   res.send("Hi"); //return Hi in browser body
-  //send status code
-  //   res.sendStatus(404);
-  //   res.sendStatus(400);
-  //   res.sendStatus(500);
-  //   res.status(400).send("your custom message");
-  //send json
-  //   res.json({
-  //     success: true,
-  //     products: [],
-  //   });
-  //send file with fs module
-  //   const file = fs.readFileSync("./index.html");
-  //   res.send(file); //not working, so i'll use path module
-  //send file with path module
-  //   const pathlocation = path.resolve();
-  //   //   console.log(path.join(pathlocation, "./index.html"));
-  //   res.sendFile(path.join(pathlocation, "./index.html"));
+  res.render("index", { name: "Rishabh" });
+  //   res.render("index.ejs");
+  //   res.sendFile("index"); //index file from public folder
+});
+
+app.get("/success", (req, res) => {
+  res.render("success");
+});
+
+app.post("/contact", (req, res) => {
+  //   console.log(req.body);
+  //   console.log(req.body.name);
+  //   console.log(req.body.email);
+  users.push({ username: req.body.name, email: req.body.email });
+  //   res.render("success");
+  res.redirect("/success");
+});
+
+app.get("/users", (req, res) => {
+  res.json({
+    users,
+  });
 });
 
 app.listen(5000, () => {
